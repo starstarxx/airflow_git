382b114266f25eb67e46451d353d947373e8ac1e
starstarxx <2499714067@qq.com>
2021-08-16 14:16:19 +0800

    微小更改，删注释

--- dag_test.py
+++ dag_test.py
@@ -1,26 +1,6 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
 
-"""
-### Tutorial Documentation
-Documentation that goes along with the Airflow tutorial located
-[here](https://airflow.apache.org/tutorial.html)
-"""
+
+
 # [START tutorial]
 # [START import_module]
 from datetime import timedelta
@@ -35,11 +15,9 @@ from airflow.utils.dates import days_ago
 
 # [END import_module]
 
-# [START default_args]
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
+
 default_args = {
-    'owner': 'airflow',
+    'owner': 'starstarxx',
     'depends_on_past': False,
     'email': ['airflow@example.com'],
     'email_on_failure': False,
