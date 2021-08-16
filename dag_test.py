6b096fe60708598c5f0e68b73e9065685a7e6fc1
starstarxx <2499714067@qq.com>
2021-08-16 16:40:59 +0800

    修改运行间隔，删除多余行

--- dag_test.py
+++ dag_test.py
@@ -1,9 +1,7 @@
-
-
-
 # [START tutorial]
 # [START import_module]
 from datetime import timedelta
+from datetime import datetime as datetime1
 from textwrap import dedent
 
 # The DAG object; we'll need this to instantiate a DAG
@@ -12,8 +10,7 @@ from airflow import DAG
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
-from airflow.operators.bash import PythonOperator
-
+from airflow.operators.python import PythonOperator
 
 
 import sys
@@ -26,11 +23,12 @@ from temp import airtask
 default_args = {
     'owner': 'starstarxx',
     'depends_on_past': False,
-    'email': ['airflow@example.com'],
+    'email': ['2499714067@qq.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
+    'start_date': datetime1(datetime1.now())
     # 'queue': 'bash_queue',
     # 'pool': 'backfill',
     # 'priority_weight': 10,
@@ -46,27 +44,18 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 # [END default_args]
-
-
 # [START instantiate_dag]
 with DAG(
-    'airflow_pipeline',
+    'air_pipeline',
     default_args=default_args,
     description='A simple tutorial DAG',
-    schedule_interval=timedelta(days=1),
-    start_date=days_ago(2),
+    schedule_interval=timedelta(seconds=3600),
+    #start_date=days_ago(2),
     tags=['test'],
 ) as dag:
-    # [END instantiate_dag]
-
-
-
-
-    # t1, t2 and t3 are examples of tasks created by instantiating operators
-    # [START basic_task]
     
     task = PythonOperator(
-        task_id='sleep_for_',
+        task_id='run_temp',
         python_callable=airtask,
         dag=dag,
     )
